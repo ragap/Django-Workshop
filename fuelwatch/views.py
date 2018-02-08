a21cac8de154e448b1fe01347ef81eb58b3c74c0
ragap <sudha.puttagunta@gmail.com>
2018-02-08 20:04:14 +0800

    Get distance

--- fuelwatch/views.py
+++ fuelwatch/views.py
@@ -6,57 +6,70 @@ import requests
 import json
 # To cache the the data
 from django.core.cache import cache
-from django.views.decorators.cache import cache_page
-
-
+from fuelwatch.keys import DIST_KEY
+# reg exp
+import re
 
 
 def testdata(request):
     print("inside test")
     if request.GET:
         # getlist  is to get the arguments in Array form
-        url,list_data=get_data(request.GET.getlist('Product'),request.GET.getlist('Metroregion'),request.GET.get('Tomorrow'))
+        url,list_data=get_data(request.GET.getlist('Product'),
+                               request.GET.getlist('Metroregion'),
+                               request.GET.get('Tomorrow')
+                               )
         return JsonResponse(list_data, safe=False)
     return render(request,"test.html")
+
 
 def mithrildata(request):
     print("inside dajngo")
     if request.GET:
         print(request.GET.getlist('Product'))
         print(request.GET.getlist('Region'))
-        print(request.GET.get('Tomorrow'))
+        print(request.GET.get('Tomorrow'),request.GET.get('lat'),request.GET.get('lng'))
+
 
         # getlist  is to get the arguments in Array form
-        list_data=get_data(request.GET.getlist('Product'),request.GET.getlist('Region'),request.GET.get('Tomorrow'))
+        list_data=get_data(request.GET.getlist('Product'),
+                           request.GET.getlist('Region'),
+                           request.GET.get('Tomorrow'),
+                           request.GET.get('lat'),
+                           request.GET.get('lng')
+                           );
 
-        # print(list_data)
         return JsonResponse(list_data, safe=False)
     return render(request,"mithril.html")
 
-# def getDistance(lat1,lng1,lat2,lng2):
-#
-#     latlng1 = f'{lat1},{lng1}|'
-#     latlng2 = f'{lat2},{lng2}'
-#
-#     x = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?',
-#                      params = {'origins':latlng1,
-#                                'destinations':latlng2,
-#                                'key':DIST_KEY})
-#
-#     #print(x.url)
-#     data= json.loads(x.content)
-#     status = [data['status']]
-#
-#     if status == ['OK']:
-#         distance = data['rows'][0]['elements'][0]['distance']['value']
-#         distancekm = distance/1000
-#         print('distance',distance)
-#         return distancekm
+def getDistance(lat1,lng1,lat2,lng2):
+
+    latlng1 = f'{lat1},{lng1}|'
+    latlng2 = f'{lat2},{lng2}'
+
+    x = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?',
+                     params = {'origins':latlng1,
+                               'destinations':latlng2,
+                               'key':DIST_KEY})
+
+    #print(x.url)
+    data= json.loads(x.content)
+    status = [data['status']]
+
+    if status == ['OK']:
+        distance = data['rows'][0]['elements'][0]['distance']['value']
+        distancekm = distance/1000
+    return distancekm
+
+def  get_safe_address(address):
+    safe_address = re.sub('[^0-9a-zA-Z]','',address)
+    return safe_address
 
 #@cache_page(None, key_prefix="Fuelwatchdata")
-def get_data(Product,Metroregion,Tomorrow):
+def get_data(Product,Metroregion,Tomorrow,use_lat,use_lng):
      # Declaring emply list to store feed data
      list_data =[]
+     address = 'str'
 
      #Get the List of URL's to Parse
      url = generate_url([Product],[Metroregion],Tomorrow)
@@ -74,21 +87,17 @@ def get_data(Product,Metroregion,Tomorrow):
 
         # item elements are available in x.entries
         for i in list_parse.entries:
+            #Call fn to replace extra char to space
+            address = get_safe_address(i.address)
 
+            distance =cache.get('latlng'+address);
 
-
-            cache.set_many({'Price' : i.price,
-                            'Location':i.location,
-                            'Address':i.address,
-                            'Phone':i.phone,
-                            'Brand' :i.brand,
-                            'Date' : i.date,
-                            'Latitude':i.latitude,
-                            'Longitude':i.longitude})
-
-            x = cache.get_many({'Address','Latitude','Longitude'})
-            # Call getDistance to send address and calculate the distance
-            #distance= getDistance(lat1,lng1,i.latitude,i.longitude)
+            if not distance:            
+                # cache the value
+                cal_dist = getDistance(use_lat,use_lng,i.latitude,i.longitude)
+                cache.set('latlng'+address,cal_dist)
+                distance = cal_dist;
+                print('elsedist',distance)
 
             dic_data = {
                         'Price' : i.price,
@@ -98,12 +107,12 @@ def get_data(Product,Metroregion,Tomorrow):
                         'Brand' :i.brand,
                         'Date' : i.date,
                         'Latitude':i.latitude,
-                        'Longitude':i.longitude
+                        'Longitude':i.longitude,
+                        'Distance':distance,
                        }
 
             list_data.append(dic_data)
-            list_data = sorted(list_data, key=lambda k: k['Price'])
-
+        list_data = sorted(list_data, key=lambda k: k['Price'])
      return list_data
 
 def generate_url(Product,Region,Tomorrow):
